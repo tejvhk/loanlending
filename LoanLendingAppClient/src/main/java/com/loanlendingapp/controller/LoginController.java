247dcd66c9bfbc7c2b5683a81ab9558d8cdf406d
tejvhk <tejvhk@gmail.com>
2016-04-03 20:31:15 +0530

    client commit

---
+++ LoanLendingAppClient/src/main/java/com/loanlendingapp/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.loanlendingapp.controller;
+
+public class LoginController {
+	private String userName;
+	private String password;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String login(){
+		System.out.println("called");
+		return password;
+	}
+
+}
